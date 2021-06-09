4503d4a2d3f09eb14cd118371fb7bda5becd79fe
Jade Westover <jade424433@gmail.com>
2021-06-08 21:51:21 -0700

    set up server

---
+++ server/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Well done!');
+})
+
+console.log("Hello")
+
+app.listen(4000, () => {
+    console.log('The application is listening on port 3000!');
+})
---
+++ server/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send('Well done!');
+});
+console.log("Hello");
+app.listen(3000, function () {
+    console.log('The application is listening on port 3000!');
+});
+//# sourceMappingURL=app.js.map
