bceb2545910d8e85b2b72d328c32c697cbc04265
Jade <jade424433@gmail.com>
2021-06-14 16:29:42 -0700

    added socket.io

--- client/src/StartGame/StartGame.tsx
+++ client/src/StartGame/StartGame.tsx
@@ -54,6 +54,4 @@ function StartPage() {
     }
 }
 
-
-
 export default StartPage
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.send('Well done!');
 })
 
-console.log("Hello")
-
-app.listen(4000, () => {
-    console.log('The application is listening on port 4000!');
-})
+io.on('connection', (socket: any) => {
+    console.log('a user connected');
+});
+  
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
